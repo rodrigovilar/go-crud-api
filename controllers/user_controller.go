ff8273cf3a43b568ede8c39729da67f380cfadff
Rodrigo Vilar <rvilar@gmail.com>
2025-07-08 07:38:32 -0300

    Add Swag docs

--- controllers/user_controller.go
+++ controllers/user_controller.go
@@ -8,12 +8,30 @@ import (
 	"github.com/rodrigovilar/go-crud-api/models"
 )
 
+// GetUsers godoc
+// @Summary Get all users
+// @Description Retrieve a list of all users
+// @Tags users
+// @Accept json
+// @Produce json
+// @Success 200 {array} models.User
+// @Router /users [get]
 func GetUsers(c *gin.Context) {
 	var users []models.User
 	database.DB.Find(&users)
 	c.JSON(http.StatusOK, users)
 }
 
+// CreateUser godoc
+// @Summary Create a new user
+// @Description Create a new user with the provided information
+// @Tags users
+// @Accept json
+// @Produce json
+// @Param user body models.User true "User object"
+// @Success 201 {object} models.User
+// @Failure 400 {object} map[string]interface{}
+// @Router /users [post]
 func CreateUser(c *gin.Context) {
 	var user models.User
 	if err := c.ShouldBindJSON(&user); err != nil {
@@ -24,6 +42,16 @@ func CreateUser(c *gin.Context) {
 	c.JSON(http.StatusCreated, user)
 }
 
+// GetUserByID godoc
+// @Summary Get user by ID
+// @Description Retrieve a user by their ID
+// @Tags users
+// @Accept json
+// @Produce json
+// @Param id path int true "User ID"
+// @Success 200 {object} models.User
+// @Failure 404 {object} map[string]interface{}
+// @Router /users/{id} [get]
 func GetUserByID(c *gin.Context) {
 	var user models.User
 	id := c.Param("id")
@@ -34,6 +62,18 @@ func GetUserByID(c *gin.Context) {
 	c.JSON(http.StatusOK, user)
 }
 
+// UpdateUser godoc
+// @Summary Update user
+// @Description Update an existing user by ID
+// @Tags users
+// @Accept json
+// @Produce json
+// @Param id path int true "User ID"
+// @Param user body models.User true "User object"
+// @Success 200 {object} models.User
+// @Failure 400 {object} map[string]interface{}
+// @Failure 404 {object} map[string]interface{}
+// @Router /users/{id} [put]
 func UpdateUser(c *gin.Context) {
 	var user models.User
 	id := c.Param("id")
@@ -49,6 +89,16 @@ func UpdateUser(c *gin.Context) {
 	c.JSON(http.StatusOK, user)
 }
 
+// DeleteUser godoc
+// @Summary Delete user
+// @Description Delete a user by ID
+// @Tags users
+// @Accept json
+// @Produce json
+// @Param id path int true "User ID"
+// @Success 200 {object} map[string]interface{}
+// @Failure 404 {object} map[string]interface{}
+// @Router /users/{id} [delete]
 func DeleteUser(c *gin.Context) {
 	var user models.User
 	id := c.Param("id")
--- main.go
+++ main.go
@@ -4,15 +4,35 @@ import (
 	"github.com/gin-gonic/gin"
 	"github.com/rodrigovilar/go-crud-api/config"
 	"github.com/rodrigovilar/go-crud-api/database"
+	_ "github.com/rodrigovilar/go-crud-api/docs"
 	"github.com/rodrigovilar/go-crud-api/routes"
+	swaggerFiles "github.com/swaggo/files"
+	ginSwagger "github.com/swaggo/gin-swagger"
 )
 
+// @title           Go CRUD API
+// @version         1.0
+// @description     A simple CRUD API built with Go, Gin, and GORM
+// @termsOfService  http://swagger.io/terms/
+
+// @contact.name   API Support
+// @contact.url    http://www.swagger.io/support
+// @contact.email  support@swagger.io
+
+// @license.name  Apache 2.0
+// @license.url   http://www.apache.org/licenses/LICENSE-2.0.html
+
+// @host      localhost:8080
+// @BasePath  /
+
+// @securityDefinitions.basic  BasicAuth
 func main() {
 	config.LoadEnv()
 	database.Connect()
 
 	r := gin.Default()
 	routes.RegisterRoutes(r)
+	r.GET("/swagger/*any", ginSwagger.WrapHandler(swaggerFiles.Handler))
 
 	r.Run()
 }
